a249f18c442f5f39863e55775bd8814f2646bb2e
raghadhawat <hawatraghad123@gmail.com>
2024-02-19 02:03:54 +0300

    refactor use case

--- lib/features/home/domain/use_cases/fetch_featured_books_use_cases.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_cases.dart
@@ -3,13 +3,15 @@ import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+import '../../../../core/use_cases/no_param_use_case.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  /// like check permission
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
