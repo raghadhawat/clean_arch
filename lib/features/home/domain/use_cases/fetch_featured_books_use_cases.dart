231c4ce4d33493a8cd6e1743ab45830963e461cd
raghadhawat <hawatraghad123@gmail.com>
2024-02-19 01:35:07 +0300

    create Fetch Featured Books Use Case

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_cases.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+
+  /// like check permission
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
